de940533fbb072cc4b21d5c9258738918bd63d77
dbenfon341 <dbenfon341@g.educaand.es>
2024-01-12 00:50:28 +0100

    Added Tablas code

--- Ejercicio_Tabla/src/Main.kt
+++ Ejercicio_Tabla/src/Main.kt
@@ -1,14 +1,74 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+/**
+ * Solicita un número entero en un rango.
+ *
+ * @param min Int - valor mínimo
+ * @param max Int - valor máximo
+ *
+ * @return Int - número entero válido dentro del rango especificado
+ */
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun pedirNum(min: Int, max: Int): Int {
+    var numero: Int = min - 1
+
+    do {
+        print("Introduzca un número entre $min y $max: ")
+        val input = readLine()
+
+        try {
+            numero = input?.toInt() ?: throw NumberFormatException()
+        } catch (e: NumberFormatException) {
+            print("**Error** Número no válido (pulse ENTER para continuar...)")
+            readLine()
+            continue
+        }
+
+        if (numero < min || numero > max) {
+            print("**Error** Número no válido (pulse ENTER para continuar...)")
+            readLine()
+        }
+
+    } while (numero < min || numero > max)
+
+    return numero
+}
+
+/**
+ * Realiza una pregunta para contestar con s/si ó n/no
+ *
+ * @param text String - Texto de la pregunta
+ *
+ * @return Boolean - true/false dependiendo de la respuesta válida a la pregunta
+ */
+
+fun pregunta(text: String): Boolean {
+    print("$text (s/n): ")
+    val respuesta = readLine()?.toLowerCase()
+
+    return when (respuesta) {
+        "s", "si" -> true
+        "n", "no" -> false
+        else -> {
+            print("**Respuesta no válida** (pulse ENTER para continuar...)")
+            readLine()
+            pregunta(text)
+        }
     }
+}
+
+fun main() {
+    do {
+        val numero = pedirNum(1, 100)
+
+        val tablaMultiplicar = Array(10) { i -> (i + 1) * numero }
+
+        println("La tabla de multiplicar del $numero: es")
+
+        for (i in 1..10) {
+            println("$i -> $numero x $i = ${tablaMultiplicar[i - 1]}")
+        }
+
+        val otraTabla = pregunta("¿Deseas generar otra tabla de multiplicación?")
+    } while (otraTabla)
+
+    println("CERRANDO")
 }
